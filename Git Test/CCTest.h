6113ab95b033b9e0322ff30b4e3c4d1e2f10e5cc
Giovanni B. Saccone <giosaccone@MacBook-Pro-di-GioSaccone.local>
2014-05-09 16:11:58 +0200

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Giovanni B. Saccone on 09/05/14.
+//  Copyright (c) 2014 Giovanni B. Saccone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Giovanni B. Saccone on 09/05/14.
+//  Copyright (c) 2014 Giovanni B. Saccone. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
